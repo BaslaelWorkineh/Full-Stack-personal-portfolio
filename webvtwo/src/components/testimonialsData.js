7127399d623465c2ce97a904971016a6d9eacf72
Baslael Workineh Ayele <64444969+BaslaelWorkineh@users.noreply.github.com>
2024-12-08 20:19:41 +0300

    Update testimonialsData.js

--- webvtwo/src/components/testimonialsData.js
+++ webvtwo/src/components/testimonialsData.js
@@ -1,7 +1,7 @@
 const testimonialsData = [
   {
     logo: "https://upload.wikimedia.org/wikipedia/commons/a/a6/Wolaita_sodo_university.png",
-    userImage: "https://media.licdn.com/dms/image/D4D03AQESFbSIZqS-NA/profile-displayphoto-shrink_200_200/0/1694724672032?e=1715212800&v=beta&t=aJS4TUUcJ4BoCFK6IrufVTh_WNFjAOk13MtOr5I1TVM",
+    userImage: "https://media.licdn.com/dms/image/v2/D4D03AQESFbSIZqS-NA/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1694724672032?e=1739404800&v=beta&t=O6AwrX9Pp8gJLZIDinoUrGJSoTLjcsXMXsFl1zmB6Ak",
     quote: 'Working with Baslael was interesting. He understood the assignment very well',
     name: 'Lidia Mekuanint',
     designation: 'Lecturer: Wolaita Sodo University'
@@ -9,7 +9,7 @@ const testimonialsData = [
   ,
   {
     logo: "https://media.licdn.com/dms/image/C4E03AQEDYJ996YZkgg/profile-displayphoto-shrink_200_200/0/1648205592782?e=1715212800&v=beta&t=ou5ZfpRTqF6o8eGhH0_kZJ11Qwx04L42hx1fCaMlOrQ",
-    userImage: "https://media.licdn.com/dms/image/C4E03AQEDYJ996YZkgg/profile-displayphoto-shrink_200_200/0/1648205592782?e=1715212800&v=beta&t=ou5ZfpRTqF6o8eGhH0_kZJ11Qwx04L42hx1fCaMlOrQ",
+    userImage: "https://media.licdn.com/dms/image/v2/D4E0BAQHeEjcWSFifWw/company-logo_100_100/company-logo_100_100/0/1722195770595?e=1741824000&v=beta&t=S83drTJKNWtuhMOgHjDezmXqjYsYkNQ2R1e8Jhy8XfI",
     quote: 'I am very pleased with Baslael\'s. work as data organizer. He did his job perfectly!',
     name: 'Misgana Workineh',
     designation: 'Founder of JarsoAI'
